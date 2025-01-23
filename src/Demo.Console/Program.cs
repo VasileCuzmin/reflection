f150b77beab2069839eac8c9ecf34bbb07d103b5
vasi <vasilecuzmin@gmail.com>
2025-01-23 22:43:09 +0200

    binding flags

--- src/Demo.Console/Program.cs
+++ src/Demo.Console/Program.cs
@@ -20,6 +20,18 @@ foreach (var method in personType?.GetMethods())
     Console.WriteLine(method);
 }
 
+foreach (var method in personType?
+    .GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+{
+    Console.WriteLine(method);
+}
+
+foreach (var method in personType?
+    .GetFields(BindingFlags.Instance | BindingFlags.NonPublic))
+{
+    Console.WriteLine(method);
+}
+
 var externalAssembly = Assembly.Load("System.Text.Json");
 var typesFromExternalAssembly = externalAssembly.GetTypes();
 var oneTypeFromExternalAssembly = externalAssembly.GetType("System.Text.Json.JsonProperty");
